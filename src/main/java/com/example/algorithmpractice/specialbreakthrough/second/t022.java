99e3d6b39be397c7e7045168b27cf686778115b6
YuanLi001 <1660665261@qq.com>
2022-08-08 16:38:21 +0800

    t022

---
+++ src/main/java/com/example/algorithmpractice/specialbreakthrough/second/t022.java
@@ -0,0 +1,34 @@
+package com.example.algorithmpractice.specialbreakthrough.second;
+
+//import com.example.algorithmpractice.TheSwordFingerOffer.SwordFingerOffer.ListNode;
+
+import java.util.HashSet;
+
+/**
+ * @author YuanLi
+ * @version 1.0
+ * @date 2022/8/8 15:40
+ */
+public class t022 {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> set=new HashSet<>();
+        while (head!=null){
+            if(set.contains(head)){
+                return head;
+            }
+            set.add(head);
+            head=head.next;
+        }
+        return null;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
