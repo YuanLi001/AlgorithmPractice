a09ca694c3ef5b573d253e0142865f11242cbc5e
YuanLi001 <1660665261@qq.com>
2022-08-24 15:40:48 +0800

    t041

---
+++ src/main/java/com/example/algorithmpractice/specialbreakthrough/four/t40.java
@@ -0,0 +1,43 @@
+package com.example.algorithmpractice.specialbreakthrough.four;
+
+/**
+ * @author YuanLi
+ * @version 1.0
+ * @date 2022/8/24 9:50
+ */
+public class t40 {
+    public int maximalRectangle(String[] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].length();
+        int[][] left = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i].charAt(j) == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i].charAt(j) == '0') {
+                    continue;
+                }
+                int width = left[i][j];
+                int area = width;
+                for (int k = i - 1; k >= 0; k--) {
+                    width = Math.min(width, left[k][j]);
+                    area = Math.max(area, (i - k + 1) * width);
+                }
+                ret = Math.max(ret, area);
+            }
+        }
+        return ret;
+    }
+
+}
---
+++ src/main/java/com/example/algorithmpractice/specialbreakthrough/four/t40_II.java
@@ -0,0 +1,60 @@
+package com.example.algorithmpractice.specialbreakthrough.four;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author YuanLi
+ * @version 1.0
+ * @date 2022/8/24 9:55
+ */
+public class t40_II {
+    public int maximalRectangle(String[] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = matrix[0].length();
+        int[][] left = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i].charAt(j) == '1') {
+                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        int ret = 0;
+        for (int j = 0; j < n; j++) { // 对于每一列，使用基于柱状图的方法
+            int[] up = new int[m];
+            int[] down = new int[m];
+
+            Deque<Integer> stack = new ArrayDeque<Integer>();
+            for (int i = 0; i < m; i++) {
+                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
+                    stack.pop();
+                }
+                up[i] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(i);
+            }
+            stack.clear();
+            for (int i = m - 1; i >= 0; i--) {
+                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
+                    stack.pop();
+                }
+                down[i] = stack.isEmpty() ? m : stack.peek();
+                stack.push(i);
+            }
+
+            for (int i = 0; i < m; i++) {
+                int height = down[i] - up[i] - 1;
+                int area = height * left[i][j];
+                ret = Math.max(ret, area);
+            }
+        }
+        return ret;
+    }
+
+
+}
