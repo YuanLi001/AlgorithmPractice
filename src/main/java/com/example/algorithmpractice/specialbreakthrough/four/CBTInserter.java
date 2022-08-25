dd438a194413f755473a9c04b87ea164be241055
YuanLi001 <1660665261@qq.com>
2022-08-25 11:12:30 +0800

    CBTInserter

---
+++ src/main/java/com/example/algorithmpractice/specialbreakthrough/four/CBTInserter.java
@@ -0,0 +1,34 @@
+//package com.example.algorithmpractice.specialbreakthrough.four;
+//
+///**
+// * @author YuanLi
+// * @version 1.0
+// * @date 2022/8/25 8:49
+// */
+//public class CBTInserter {
+//    public CBTInserter(TreeNode root) {
+//
+//    }
+//
+//    public int insert(int v) {
+//
+//    }
+//
+//    public TreeNode get_root() {
+//
+//    }
+//}
+//
+//class TreeNode {
+//      int val;
+//      TreeNode left;
+//      TreeNode right;
+//      TreeNode() {}
+//      TreeNode(int val) { this.val = val; }
+//      TreeNode(int val, TreeNode left, TreeNode right) {
+//          this.val = val;
+//          this.left = left;
+//          this.right = right;
+//      }
+//}
+//
