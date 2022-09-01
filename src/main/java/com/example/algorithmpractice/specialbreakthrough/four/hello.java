c808e08b3e8ae5cf5d2b8ba747db21403d5c07ac
YuanLi001 <1660665261@qq.com>
2022-09-01 09:48:53 +0800

    morris

---
+++ src/main/java/com/example/algorithmpractice/specialbreakthrough/four/hello.java
@@ -0,0 +1,12 @@
+package com.example.algorithmpractice.specialbreakthrough.four;
+
+/**
+ * @author YuanLi
+ * @version 1.0
+ * @date 2022/8/31 20:26
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
