c57ab608484daa9489253a7b1433e82a6e6cbf17
YuanLi001 <1660665261@qq.com>
2022-10-10 20:20:20 +0800

    UniquePaths_DP

--- src/main/java/com/example/algorithmpractice/specialbreakthrough/seven/UniquePaths.java
+++ src/main/java/com/example/algorithmpractice/specialbreakthrough/seven/UniquePaths.java
@@ -6,6 +6,13 @@ package com.example.algorithmpractice.specialbreakthrough.seven;
  * @date 2022/10/10 19:26
  */
 public class UniquePaths {
+//   2       6
+// c       c
+//   3       7
+
+//  2                (m-1)
+// c    8*7/2      c
+//  8                 (m+n-2)
     public int uniquePaths(int m, int n) {
         int xia=m+n-2;
 //         shang=m-1;
