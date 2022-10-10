fd38f742e3c58ad196aaecd95f74dfefaa3bfe43
YuanLi001 <1660665261@qq.com>
2022-10-10 19:18:08 +0800

    numDistinct

--- src/main/java/com/example/algorithmpractice/specialbreakthrough/seven/NumDistinct.java
+++ src/main/java/com/example/algorithmpractice/specialbreakthrough/seven/NumDistinct.java
@@ -7,6 +7,20 @@ package com.example.algorithmpractice.specialbreakthrough.seven;
  */
 public class NumDistinct {
     public int numDistinct(String s, String t) {
-
+        int sLen=s.length(),tLen=t.length();
+        int[][]  dp=new int[sLen+1][tLen+1];
+        for (int i = 0; i <= sLen; i++) {
+            dp[i][0]=1;
+        }
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= tLen ; j++) {
+                if(s.charAt(i-1)==t.charAt(j-1) ){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[sLen][tLen];
     }
 }
