981e2a7c34c2cd27d1e7b54da92676e9410bbc4c
YuanLi001 <1660665261@qq.com>
2022-11-01 15:50:31 +0800

    CalcEquation

---
+++ src/main/java/com/example/algorithmpractice/specialbreakthrough/ten/qwe.java
@@ -0,0 +1,14 @@
+package com.example.algorithmpractice.specialbreakthrough.ten;
+
+/**
+ * @author YuanLi
+ * @version 1.0
+ * @date 2022/11/1 11:12
+ */
+public class qwe {
+    public static void main(String[] args) {
+//        int a=1;
+        int a=1;
+        System.out.println(a);
+    }
+}
