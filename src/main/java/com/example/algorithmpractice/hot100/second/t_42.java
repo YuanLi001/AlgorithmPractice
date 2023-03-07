be8d28112458b9e1a03f3f6c9a0310d681a81526
YuanLi001 <1660665261@qq.com>
2023-03-07 18:54:32 +0800

    t42

--- src/main/java/com/example/algorithmpractice/hot100/second/t_42.java
+++ src/main/java/com/example/algorithmpractice/hot100/second/t_42.java
@@ -7,24 +7,6 @@ package com.example.algorithmpractice.hot100.second;
  */
 public class t_42 {
 
-//    public int trap(int[] height) {
-//        int ans = 0;
-//        int left = 0, right = height.length - 1;
-//        int leftMax = 0, rightMax = 0;
-//        while (left < right) {
-//            leftMax = Math.max(leftMax, height[left]);
-//            rightMax = Math.max(rightMax, height[right]);
-//            if (height[left] < height[right]) {
-//                ans += leftMax - height[left];
-//                ++left;
-//            } else {
-//                ans += rightMax - height[right];
-//                --right;
-//            }
-//        }
-//        return ans;
-//    }
-
     public int trap(int[] height) {
         int ans = 0;
         int left = 0, right = height.length - 1;
@@ -35,7 +17,7 @@ public class t_42 {
             if (height[left] < height[right]) {
                 ans += leftMax - height[left];
                 ++left;
-            }else {
+            } else {
                 ans += rightMax - height[right];
                 --right;
             }
