ddeeacede05df2f3b094199b3d8961abda04af7d
YuanLi001 <1660665261@qq.com>
2023-03-08 21:50:27 +0800

    t48

---
+++ src/main/java/com/example/algorithmpractice/hot100/second/t_48.java
@@ -0,0 +1,30 @@
+package com.example.algorithmpractice.hot100.second;
+
+/**
+ * @author YuanLi
+ * @version 1.0
+ * @date 2023/3/8 21:47
+ */
+public class t_48 {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+}
